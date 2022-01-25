8abb7116661d97f3d38fc42701218662338e29b9
Fabian Kaltwasser <fabian.kaltwasser@rub.de>
2022-01-25 14:29:00 +0100

    Added RadioItems for Zones
    Implementation of Heartratezones still WIP

--- dash_server/pages/speedzones_page.py
+++ dash_server/pages/speedzones_page.py
@@ -16,24 +16,74 @@ from plotly.subplots import make_subplots
 
 from app import app
 
-layout = html.Div([
-    html.H1("Speedzones:"),
-    html.Label("Seperate speedzones by ',':"),
-    html.Br(),
-    dcc.Input(id="speedzone-input", value="0,5,15,20,30"),
-    dcc.Graph(
-        id='speedzones-graph',
-        config= {'displayModeBar': False, 'scrollZoom': False, },
-        #layout = go.Layout(template='plotly_dark')
+layout = html.Div(id='zonelayout',children=[
+    html.H1("Zones:"),
+    dcc.RadioItems(
+        id="zone_radio",
+        options=[
+            {'label': 'Speedzones', 'value': 'speed'},
+            {'label': 'heartratezones', 'value': 'heartrate'}
+        ],
+        #value='speed',
     ),
-    html.Button('Generate', id='gen_speedzones', n_clicks=0),
+    html.Div(id="zone_graph",children=[])
 ])
+
+@app.callback(
+    Output('zone_graph','children'),
+    Input('zone_radio','value')
+)
+def choose_zone(choosen_zone):
+    if choosen_zone == 'speed':
+            speedlayout = [html.Div([
+            html.H1("Speedzones:"),
+            html.Label("Seperate speedzones by ',':"),
+            html.Br(),
+            dcc.Input(id="speedzone-input", value="0,5,15,20,30"),
+            dcc.Graph(
+                id='speedzones-graph',
+                config= {'displayModeBar': False, 'scrollZoom': False, },
+                #layout = go.Layout(template='plotly_dark')
+            ),
+            html.Button('Generate', id='gen_speedzones', n_clicks=0)])]
+            return speedlayout
+    if choosen_zone == 'heartrate':
+            heartlayout = [html.Div([
+            html.H1("Heartratezones:"),
+            html.Label("Seperate Heartratezones by ',':"),
+            html.Br(),
+            dcc.Input(id="heartratezone-input", value="0,5,15,20,30"),
+            dcc.Graph(
+                id='heartratezones-graph',
+                config= {'displayModeBar': False, 'scrollZoom': False, },
+                #layout = go.Layout(template='plotly_dark')
+            ),
+            html.Button('Generate', id='gen_heartratezones', n_clicks=0)])]
+            return heartlayout
+    else:
+        raise PreventUpdate
+
+#Insert heartratecallback here
+"""
+@app.callback(
+    Output("heartratezones-graph", "figure"),
+    Input("gen_heartratezones", "n_clicks"),
+    State("heartratezone-input", "value"))
+def update_heartratezone_figure(n_clicks, heartzone_input):
+    df = am.df
+    print(df.keys())
+    heartzones = []
+    temp_heartzones = heartzone_input.split(',')
+    for zone in temp_heartzones:
+        heartzones.append(float(zone.strip()))
+    return 'lul' """
+
 
 @ app.callback(
     Output("speedzones-graph", "figure"),
     Input("gen_speedzones", "n_clicks"),
     State("speedzone-input", "value"))
-def update_figure(n_clicks, speedzone_input):
+def update_speedzone_figure(n_clicks, speedzone_input):
     df = am.df
 
     zones = []
