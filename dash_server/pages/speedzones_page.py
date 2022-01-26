a58cce9c28ecfe9befcf5bf7e9b86925ec84af98
Fabian Kaltwasser <fabian.kaltwasser@rub.de>
2022-01-26 15:43:27 +0100

    Added 'radio' for relativ/total time spend in zones

--- dash_server/pages/speedzones_page.py
+++ dash_server/pages/speedzones_page.py
@@ -1,4 +1,5 @@
 from inspect import Parameter
+from multiprocessing.sharedctypes import Value
 import time
 import dash as ds
 from dash.dependencies import Output, Input, State
@@ -25,54 +26,84 @@ layout = html.Div(id='zonelayout',children=[
             {'label': 'Speedzones', 'value': 'speed'},
             {'label': 'heartratezones', 'value': 'heartrate'}
         ],
-        #value='speed',
+        value='speed'
     ),
     html.Div(id="zone_graph",children=[
         html.Div([
-            html.H1("Speedzones:"),
-            html.Label("Seperate speedzones by ',':"),
-            html.Br(),
-            dcc.Input(id="speedzone-input", value="0,5,15,20,30"),
-            dcc.Loading(
-                id="loading-1",
-                type="default",
-                children=[
-                    dcc.Graph(
-                        id='speedzones-graph',
-                        config= {'displayModeBar': False},
-                        #layout = go.Layout(template='plotly_dark')
-                    )
-                ]
-            )
-        ])
-    ]),
-    dcc.Store(
-        id="visible-players",
-        data=[]
-    )
+                html.H3("Relativ Percentage or Total time spend in Minutes?"),
+                dcc.RadioItems(
+                    id='relortot',
+                    options=[
+                        {'label': 'Relativ', 'value': 'rel'},
+                        {'label': 'Total', 'value':'tot'}
+                    ],
+                    value='rel'
+                ),
+                html.Label("Seperate zones by ',':"),
+                html.Br(),
+                dcc.Input(id="speedzone-input", value="0,5,15,20,30"),
+                dcc.Loading(
+                    id="loading-1",
+                    type="default",
+                    children=[
+                        dcc.Graph(
+                            id='speedzones-graph',
+                            config= {'displayModeBar': False},
+                            
+                            )
+                    ]
+                )
+            ]),
+        dcc.Store(
+            id="visible-players",
+            data=[]
+        )
+    ])
 ])
-
-
+"""
+@app.callback(
+    Output('zone_graph', 'children'),
+    Input('zone_radio', 'value')
+)
 def choose_zone(choosen_zone):
     if choosen_zone == 'speed':
             speedlayout = [
-                
+                html.Div([
+                    html.H1("Speedzones:"),
+                    html.Label("Seperate speedzones by ',':"),
+                    html.Br(),
+                    dcc.Input(id="speedzone-input", value="0,5,15,20,30"),
+                    dcc.Loading(
+                        id="loading-1",
+                        type="default",
+                        children=[
+                            dcc.Graph(
+                                id='speedzones-graph',
+                                config= {'displayModeBar': False},
+                                #layout = go.Layout(template='plotly_dark')
+                            )
+                        ]
+                    )
+                ])
             ]
             return speedlayout
     if choosen_zone == 'heartrate':
-            heartlayout = [html.Div([
-            html.H1("Heartratezones:"),
-            html.Label("Seperate Heartratezones by ',':"),
-            html.Br(),
-            dcc.Input(id="heartratezone-input", value="0,5,15,20,30"),
-            dcc.Graph(
-                id='heartratezones-graph',
-                config= {'displayModeBar': False},
-                #layout = go.Layout(template='plotly_dark')
-            )])]
+            heartlayout = [
+                html.Div([
+                    html.H1("Heartratezones:"),
+                    html.Label("Seperate Heartratezones by ',':"),
+                    html.Br(),
+                    dcc.Input(id="heartratezone-input", value="0,5,15,20,30"),
+                    dcc.Graph(
+                        id='heartratezones-graph',
+                        config= {'displayModeBar': False},
+                        #layout = go.Layout(template='plotly_dark')
+                    )])]
             return heartlayout
     else:
         raise PreventUpdate
+
+"""
 
 #Insert heartratecallback here
 """
@@ -95,12 +126,12 @@ def update_heartratezone_figure(n_clicks, heartzone_input):
     Output('visible-players', 'data'),
     Input("zone_radio", "value"),
     Input('example-graph', 'restyleData'),
+    
     State("speedzone-input", "value"),
-    State('visible-players', 'data'))
-def update_speedzone_figure(zone_type, speed_graph, speedzone_input, visible_players):
+    State('visible-players', 'data'),
+    Input('relortot', 'value'))
+def update_speedzone_figure(zone_type, speed_graph, speedzone_input, visible_players,relativortotal):
     df = am.df
-    print("draw speedzones")
-
     zones = []
     temp_speedzones = speedzone_input.split(',')
     for zone in temp_speedzones:
@@ -109,29 +140,53 @@ def update_speedzone_figure(zone_type, speed_graph, speedzone_input, visible_pla
     if "speed" not in df:
         raise PreventUpdate
 
+
     total = df.groupby('playerID').speed.count()
     speedzones = []
+    
     for index, zone  in enumerate(zones):
         if index < len(zones)-1:
-            speedzones.append(
-                df[
-                    (df['speed'] >= zones[index]) & (df['speed'] < zones[index+1])
-                ].groupby('playerID')
-                .speed
-                .count() / total
+            if(relativortotal == 'rel'):
+                speedzones.append(
+                    df[
+                        (df['speed'] >= zones[index]) & (df['speed'] < zones[index+1])
+                    ].groupby('playerID')
+                    .speed
+                    .count() / total
+                )
+            if(relativortotal == 'tot'):
+                speedzones.append(
+                    df[
+                        (df['speed'] >= zones[index]) & (df['speed'] < zones[index+1])
+                    ].groupby('playerID')
+                    .speed
+                    .count()/60
             )
+            if(relativortotal == ''):
+                raise PreventUpdate
         else:
-            speedzones.append(
-                df[
-                    (df['speed'] >= zones[index]) & (df['speed'] < np.inf)
-                ].groupby('playerID')
-                .speed
-                .count() / total
-            )
+            if(relativortotal == 'rel'):
+                speedzones.append(
+                    df[
+                        (df['speed'] >= zones[index]) & (df['speed'] < np.inf)
+                    ].groupby('playerID')
+                    .speed
+                    .count() / total
+                )
+            if(relativortotal == 'tot'):
+                speedzones.append(
+                    df[
+                        (df['speed'] >= zones[index]) & (df['speed'] < np.inf)
+                    ].groupby('playerID')
+                    .speed
+                    .count()/60
+                )
+
+            if(relativortotal == ''):
+                raise PreventUpdate
+    
 
     speedzones_df = pd.DataFrame([speedzone.array for speedzone in speedzones], columns=speedzones[0].index.array)
-
-    print(speed_graph)
 
     for i in range(len(visible_players), len(speedzones[0].index.array)):
         visible_players.append(True)
@@ -148,20 +203,13 @@ def update_speedzone_figure(zone_type, speed_graph, speedzone_input, visible_pla
                 visible_players[index] = is_visibale
         except KeyError:
             pass
-
-    print(visible_players)
-
     players = speedzones[0].index.array[visible_players]
-
-    print(players)
-    print(int(len(players)/3))
 
     fig = px.bar(speedzones_df)
 
     fig = make_subplots(rows=int(np.ceil(len(players)/3)), cols=3, subplot_titles=players)
 
     for index, player in enumerate(players):
-        print((index) % 3 + 1, int(np.ceil((index+1)/3)))
         fig.add_trace(go.Bar(x=speedzones_df[player], y=speedzones_df.index + 1, marker_color=speedzones_df.index, orientation="h"), row=int(np.ceil((index+1)/3)), col=(index) % 3 + 1)
 
     fig.update_xaxes(
